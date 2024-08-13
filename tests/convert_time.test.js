dc2ee3df46ff4ce2227f8feac4c7f6b4ee4ed6df
Sasha <na-okne@mail.ru>
2024-08-14 01:13:24 +0300

    добавила тесты для функции convertTime

---
+++ tests/convert_time.test.js
@@ -0,0 +1,58 @@
+import { assert } from 'chai';
+import convertTime from '../public/assets/convert_time';
+/* global describe, it */
+describe('Функция проверки расчета времени, прошедшего с момента публикации поста', function () {
+  it('разница – 60 минут', function () {
+    const expectedResult = "1 час назад";
+    const result = convertTime(new Date("2022-01-01T12:00:00Z"), new Date("2022-01-01T13:01:00Z"));
+    assert.equal(expectedResult, result);
+  });
+
+  it('разница – 24 часа', function () {
+    const expectedResult = "1 день назад";
+    const result = convertTime(new Date("2022-01-01T00:00:00Z"), new Date("2022-01-02T00:00:00Z"));
+    assert.equal(expectedResult, result);
+  });
+
+  it('разница - 365 дней', function () {
+    const expectedResult = "1 год назад";
+    const result = convertTime(new Date("2021-01-01T00:00:00Z"), new Date("2022-01-01T00:00:00Z"));
+    assert.equal(expectedResult, result);
+  });
+
+  it('разница – 1 минута', function () {
+    const expectedResult = "1 минуту назад";
+    const result = convertTime(new Date("2022-01-01T12:00:00Z"), new Date("2022-01-01T12:01:00Z"));
+    assert.equal(expectedResult, result);
+  });
+
+  it('разница – 59 минут', function () {
+    const expectedResult = "59 минут назад";
+    const result = convertTime(new Date("2022-01-01T12:00:00Z"), new Date("2022-01-01T12:59:00Z"));
+    assert.equal(expectedResult, result);
+  });
+
+  it('разница – меньше минуты', function () {
+    const expectedResult = "только что";
+    const result = convertTime(new Date("2022-01-01T12:00:00Z"), new Date("2022-01-01T12:00:30Z"));
+    assert.equal(expectedResult, result);
+  });
+
+  it('разница - 5 дней', function () {
+    const expectedResult = "5 дней назад";
+    const result = convertTime(new Date("2022-01-01T12:00:00Z"), new Date("2022-01-01T12:00:00Z"));
+    assert.equal(expectedResult, result);
+  });
+
+  it('разница – более года', function () {
+    const expectedResult = "1 год назад";
+    const result = convertTime(new Date("2022-01-01T12:00:00Z"), new Date("2022-01-01T12:00:00Z"));
+    assert.equal(expectedResult, result);
+  });
+
+  it('разница – 2 года', function () {
+	const expectedResult = "2 года назад";
+	const result = convertTime(new Date("2022-01-01T12:00:00Z"), new Date("2024-01-01T12:00:00Z"));
+	assert.equal(expectedResult, result);
+ });
+});
