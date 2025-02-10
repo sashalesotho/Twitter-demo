2f9591615758000a4ffbc01cb5dc6b8568ca3b10
Sasha <na-okne@mail.ru>
2025-02-11 01:13:17 +0300

    created token

--- vite.config.js
+++ vite.config.js
@@ -13,6 +13,7 @@ export default defineConfig({
       '/posts': 'http://localhost:3000',
       '/createUser': 'http://localhost:3000',
       '/login': 'http://localhost:3000',
+      '/protected-route': 'http://localhost:3000',
     },
   },
 });
